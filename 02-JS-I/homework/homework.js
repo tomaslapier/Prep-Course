68aa62a3ce3fe9f6b8e199a6bdac338aed5b717c
tomaslapier <tomaslapier@gmail.com>
2022-11-03 23:08:07 -0300

    prueba2

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -4,7 +4,7 @@
 const nuevaString = "hola";
 
 // Crea una variable numérica, puede ser cualquier número:
-const nuevoNum = 4;
+const nuevoNum = 5;
 
 // Crea una variable booleana:
 const nuevoBool = true;
